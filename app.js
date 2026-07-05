d52101f7169e1135eaa77d72c4cdaac916587297
ZeeshanGul404 <zeeshandiscordking@gmail.com>
2026-07-05 11:41:59 -0700

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let cart = [1200, 500, 800]; // product store karne ke liye array
 let total = 0; // total store ke liye variable
 for (let i = 0; i < cart.length; i++) {
   // loop chalaya or array ki length check ki
-  total = total + cart[i]; // each product ko total me add kiya
+  total = total + cart[i]; // each product ko total me add kiya.
 }
 
 console.log(total);
